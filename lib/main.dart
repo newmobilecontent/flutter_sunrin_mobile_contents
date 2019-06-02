81ecb101e81f4d32f257fb3a8a95aa75a40a7312
Lansional <cbg1720@gmail.com>
2019-06-02 21:39:40 +0900

    so hard

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 import 'Login.dart';
-
-//enum Login {
-//  Email,
-//  GOOGLE,
-//  Facebook,
-//  Github,
 
 void main() => runApp(MyApp());
 
@@ -25,40 +18,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-//}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text('Just Test'),
-      ),
-      body: FloatingActionButton(
-        child: Icon(Icons.access_alarm),
-        tooltip: 'Tooltip',
-        foregroundColor: Colors.white,
-        backgroundColor: Colors.deepPurple,
-        shape: const Border(),
-        onPressed: () {
-          Fluttertoast.showToast(
-            msg: 'Oh Yeah!!!~~~',
-            toastLength: Toast.LENGTH_SHORT,
-            gravity: ToastGravity.BOTTOM,
-            timeInSecForIos: 1
-          );
-        },
-      ),
-    );
-  }
-}

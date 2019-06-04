58d897b3853c1886ef1bb5a420293956d317afa6
Lansional <cbg1720@gmail.com>
2019-06-04 18:27:31 +0900

    Login.dart Page Simple success

--- lib/Login.dart
+++ lib/Login.dart
@@ -2,51 +2,79 @@ import 'package:flutter/material.dart';
 import 'package:groovin_material_icons/groovin_material_icons.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
+/// @Autor Lansional
+///     로그인 화면
+/// 로그인 페이지 순서
+/// Material -> Align -> Column -> SizedBox -> Form -> Align -> ListView
+
 class LoginPage extends StatefulWidget {
   @override
   _LoginPage createState() => _LoginPage();
 }
 
 class _LoginPage extends State<LoginPage> {
-  final _formKey = GlobalKey<FormState>();
   String getEmail, getPassWord;
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            width: 350,
-            height: 100,
+    return Scaffold(                                        // bottom을 확인 버튼
+      body: Container(
+        padding: EdgeInsets.all(40.0),
+        child: Column(
 
-            child: Form(
-              key: _formKey,
-              child: ListView(
-                children: <Widget>[
-                  TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      icon: Icon(GroovinMaterialIcons.github_box),
-                      labelText: '깃허브 이메일',
-                    ),
-                    onChanged: (i) => getEmail = i,
-                  )
-                ],
-              ),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  icon: Icon(GroovinMaterialIcons.github_face),
+                  labelText: '깃허브 이메일',
+                ),
+                onChanged: (i) => getEmail = i
             ),
-          ),
-          RaisedButton(
-              child: Text('확인'),
-              color: Colors.blue,
-              onPressed: () {
-                if (_formKey.currentState.validate()) {
-                  Fluttertoast.showToast(msg: '이메일이 아닙니다.');
-                }
-                Fluttertoast.showToast(msg: '$getEmail');
-              }
-          )
-        ],
+            TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  icon: Icon(GroovinMaterialIcons.lock),
+                  labelText: '깃허브 비번',
+                ),
+                onChanged: (i) => getPassWord = i,
+              obscureText: true,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(GroovinMaterialIcons.github_face),
+        onPressed: () {
+          Fluttertoast.showToast(msg: '$getEmail: $getPassWord');
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10.0,
+        color: Colors.blue,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(icon: Icon(Icons.menu), onPressed: () => AlertDialog(
+              title: Text('Menu'),
+              content: Text('Study'),
+              actions: <Widget>[
+                Row(
+                  children: <Widget>[
+                    ButtonBar(
+
+                    )
+                  ],
+                )
+              ],
+            )),
+            IconButton(icon: Icon(Icons.search), onPressed: () => Fluttertoast.showToast(msg: 'search'))
+          ],
+        ),
       ),
     );
   }
